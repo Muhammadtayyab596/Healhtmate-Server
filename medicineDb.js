cb812b3590e5f95bf8966809e0816d8b52a5cafd
MFarhatTayyab <tayyabazeemi786@gmail.com>
2023-07-13 11:55:23 -0700

    update file

--- medicineDb.js
+++ medicineDb.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
-const connectDB = require("./db/connect")
-const Medicine = require("./models/medicine")
-const Medicinejson = require('./medicine.json')
-
+const connectDB = require("./db/connect");
+const Medicine = require("./models/medicine");
+const Medicinejson = require("./medicine.json");
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGODB_URL);
-        await Medicine.create(Medicinejson)
-        console.log('dATA ADDED')
-    }
-    catch (error) {
-        console.log(error)
-    }
-}
+  console.log(Medicinejson);
+  // try {
+  //     await connectDB(process.env.MONGODB_URL);
+  //     await Medicine.create(Medicinejson)
+  //     console.log('dATA ADDED')
+  // }
+  // catch (error) {
+  //     console.log(error)
+  // }
+};
 
 start();
--- models/disease.js
+++ models/disease.js
@@ -5,16 +5,16 @@ const medicineSchema = new mongoose.Schema({
   medicineName: { type: String },
   medicineDetail: { type: String },
   uses: { type: String },
-  sideEffects: { type: String },
+  SideEffects: { type: String },
 });
 
 // Define the schema for the disease
 const diseaseSchema = new mongoose.Schema({
   id: { type: String },
   diseaseName: { type: String },
-  diseaseDetails: { type: String },
+  diseaseDeatils: { type: String },
   symptom: { type: String },
   medicine: [medicineSchema],
 });
 
-module.exports = mongoose.model("Disease", diseaseSchema);
+module.exports = mongoose.model("DiseaseModelUpadteAgain", diseaseSchema);
